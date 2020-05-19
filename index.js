c5db4c947f45427e00b6d2ded98e9bb80dc07576
yoneda <nsshowdream@gmail.com>
2020-05-19 23:01:11 +0900

    [add] add new typescript file

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const asyncHandler = require("express-async-handler");
-
-const server = express();
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var asyncHandler = require("express-async-handler");
+var server = express();
 server.use(cookieParser());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-
 // api を定義
-server.get("/api/helth", (req, res) => {
-  res.send({ val: "ok" });
+server.get("/api/helth", function (req, res) {
+    res.send({ val: "ok" });
 });
-
-const port = process.env.PORT;
-server.listen(port || 3000, (err) => {
-  if (err) throw err;
-  console.log(`ready on localhost:${port}`);
+var port = process.env.PORT;
+server.listen(port || 3000, function (err) {
+    if (err)
+        throw err;
+    console.log("ready on localhost:" + port);
 });
---
+++ index.ts
@@ -0,0 +1,20 @@
+const express = require("express");
+import { Request, Response } from "express";
+const cookieParser = require("cookie-parser");
+const asyncHandler = require("express-async-handler");
+
+const server = express();
+server.use(cookieParser());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+// api を定義
+server.get("/api/helth", (req: Request, res: Response) => {
+  res.send({ val: "ok" });
+});
+
+const port = process.env.PORT;
+server.listen(port || 3000, (err) => {
+  if (err) throw err;
+  console.log(`ready on localhost:${port}`);
+});
